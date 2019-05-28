7e38e62308447967712e75fde7e290d1ad95ced1
Lawrence Tran <mrlawrencetran@gmail.com>
2019-05-27 20:27:26 -0700

    Tracking Stats

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,65 @@
+var firstCardClicked = null;
+var secondCardClicked = null;
+var matches = null;
+var max_matches = 9;
+var attempts = 0;
+var games_played = 0;
+var accuracy = 0;
 
+$(document).ready(function() {
+    $('.card').on('click', handleCardClick);
+});
+
+function handleCardClick(event) {
+    $(this).find('.card-back').addClass('hidden');
+
+    if (firstCardClicked === null) {
+        firstCardClicked = $(this);
+    } else {
+        secondCardClicked = $(this);
+        $(this).find('.card-back').addClass('hidden');
+        attempts++;
+
+        var firstImage = $(firstCardClicked).find('.card-front img').attr('src');
+        var secondImage = $(secondCardClicked).find('.card-front img').attr('src');
+
+        if (firstImage === secondImage) {
+            matches++;
+            max_matches++;
+            setTimeout(matchAlert, 20);
+            firstCardClicked = null;
+            secondCardClicked = null;
+            debugger;
+            if (matches === max_matches) {
+                $('.modal').show();
+            }
+        } else {
+            displayStats();
+            calculateAccuracy();
+            setTimeout(hideCards, 1500);
+        }
+    }
+}
+
+function displayStats() {
+    $('.attempts .value').text(this.attempts);
+    $('.accuracy .value').text(this.Math.round(accuracy*100) + '%');
+    $('.games-played .value').text(this.games_played);
+}
+
+function calculateAccuracy() {
+    accuracy = matches/attempts;
+}
+
+function matchAlert() {
+    alert('Nice Match!')
+}
+
+function hideCards() {
+    firstCardClicked.find('.card-back').removeClass('hidden');
+    secondCardClicked.find('.card-back').removeClass('hidden');
+    firstCardClicked.find('.card-front').removeClass('hidden');
+    secondCardClicked.find('.card-front').removeClass('hidden');
+    firstCardClicked = null;
+    secondCardClicked = null;
+}
